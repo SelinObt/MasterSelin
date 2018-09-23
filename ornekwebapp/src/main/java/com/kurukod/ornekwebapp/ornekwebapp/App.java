31f99875ccb26cd0cecbcf9cefc5443af5043361
selinobut <selinobt@gmail.com>
2018-09-23 19:00:10 +0300

    Selin uzun zaman sobra kod yazdı.

--- ornekwebapp/src/main/java/com/kurukod/ornekwebapp/ornekwebapp/App.java
+++ ornekwebapp/src/main/java/com/kurukod/ornekwebapp/ornekwebapp/App.java
@@ -8,6 +8,17 @@ public class App
 {
     public static void main( String[] args ) 
     {
-        System.out.println( "Hello World!" ); 
+        Car bmw = new Car();
+        bmw.setName("3.20d");
+        bmw.setYear(2018);
+        
+        System.out.println(bmw.getName() + " " + bmw.getYear());
+        
+        Car reno = new Car();
+        
+        reno.setName("Laguna");
+        reno.setYear(2001);
+        
+        System.out.println(reno.getName() + " " + reno.getYear());
     }
 }
---
+++ ornekwebapp/src/main/java/com/kurukod/ornekwebapp/ornekwebapp/Car.java
@@ -0,0 +1,21 @@
+package com.kurukod.ornekwebapp.ornekwebapp;
+
+public class Car {
+	
+	private String name;
+	private int year;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+
+}
