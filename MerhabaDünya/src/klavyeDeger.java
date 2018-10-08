bd659b99772a363f30bff84930adc37d1d106b8d
selinobut <selinobt@gmail.com>
2018-10-09 00:12:15 +0300

    Methodlar

---
+++ MerhabaDünya/src/klavyeDeger.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class klavyeDeger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String siparisim="sepette";
+		siparisKontrol();
+	}
+		
+		public static void siparisKontrol () {
+			String siparisim = "sepette";
+			
+			if (siparisim == "sepette") {
+				
+				System.out.println("Alışverişe devam edebilirsin");
+			}
+			else  if(siparisim=="kargoda")  {
+				
+				System.out.println("Kargo bilgileriniz aşağıdaki gibidir");
+			}
+			
+			
+		}
+	}
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/* Scanner selin = new Scanner(System.in);
+
+		int num1, num2;
+
+		System.out.println("Birinci sayıyı giriniz  ");
+		num1 = selin.nextInt();
+		System.out.println("birinci sayı girildi   " + num1);
+		System.out.println("İkinci sayıyı giriniz ");
+		num2 = selin.nextInt();
+		System.out.println("ikinci sayıyı girildi" + num2);
+
+		if (num1 < num2) {
+
+			System.out.println("birinci sayı ikinci sayıdan büyüktür");
+		}
+
+		else if (num2 > num1) {
+			System.out.println("ikinc sayı birinci sayıdan büyüktür.");
+
+		} else {
+
+			System.out.println("sayıalr birbrine eşittir.");
+		}
+		
+		*/
